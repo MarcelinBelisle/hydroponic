0d6313cf85fb04f06c10003a3a46a21410b5766e
Marcelin <marcelinbelisle@gmail.com>
2022-02-28 11:24:43 -0500

    arraystore

--- public/firechartpH.js
+++ public/firechartpH.js
@@ -1,68 +1,74 @@
+    
 
-Chart.defaults.font.size = 20;
-Chart.defaults.font.color = '#000';
-const ctx = document.getElementById('myChartpH').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['pH value'],
-        datasets: [{
-            label: 'pH value',
-            data: [],
-            fill: true,
-            backgroundColor: 'rgb(120, 16, 126)',
-            borderWidth: 1,
-      pointStyle: 'circle',
-      pointRadius: 5,
-      pointBackgroundColor: "yellow",
-      pointBorderColor: 'rgb(0, 0, 0)'
-           
-        }]
-    },
-    options: {            
-            scales: {
-                y: {
-                  beginAtZero: true,
-                    }
-                              
-            }
+    const socket = io();
+    Chart.defaults.font.size = 20;
+    Chart.defaults.font.color = '#000';
+    const ctx = document.getElementById('myChartpH').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['pH value'],
+            datasets: [{
+                label: 'pH value',
+                data: [],
+                fill: true,
+                backgroundColor: 'rgb(120, 16, 126)',
+                borderWidth: 1,
+        pointStyle: 'circle',
+        pointRadius: 5,
+        pointBackgroundColor: "yellow",
+        pointBorderColor: 'rgb(0, 0, 0)'
+            
+            }]
         },
-    });
+        options: {            
+                scales: {
+                    y: {
+                    beginAtZero: true,
+                        }
+                                
+                }
+            },
+        });
 
-    
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-    
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAScT_b2jdH7OV-jChxxIYmuIOtxOL0Tcs",
-  authDomain: "hydro-closet.firebaseapp.com",
-  databaseURL: "https://hydro-closet-default-rtdb.firebaseio.com",
-  projectId: "hydro-closet",
-  storageBucket: "hydro-closet.appspot.com",
-  messagingSenderId: "419523459894",
-  appId: "1:419523459894:web:7b6d55c5af770e182f1197"
+apiKey: "AIzaSyAScT_b2jdH7OV-jChxxIYmuIOtxOL0Tcs",
+authDomain: "hydro-closet.firebaseapp.com",
+databaseURL: "https://hydro-closet-default-rtdb.firebaseio.com",
+projectId: "hydro-closet",
+storageBucket: "hydro-closet.appspot.com",
+messagingSenderId: "419523459894",
+appId: "1:419523459894:web:7b6d55c5af770e182f1197"
 };
 
 const app = initializeApp(firebaseConfig);
 
-import {getDatabase, ref, get, set, child, update, remove, onValue}
-from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+import {getFirestore, doc, getDoc, setDoc, collection, addDoc, updateDoc, deleteDoc, deleteField, arrayUnion}
+from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
-const db = getDatabase();
-
-let counter8 = 0;
- function addData(chart, data) {
-    chart.data.labels.push(counter8);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    counter8++;
-    chart.update();
-    }
+const db = getFirestore();
 
 
-    const dbRef = ref(db,"pHvalue");
-    onValue(dbRef,(snapshot)=>{
-        var ph = snapshot.val().pH;
-        addData(myChart, ph);
-    });
 
+let counter = 0;
+
+socket.on('arduino:data', function (dataSerial) {
+myChart.data.labels.push(counter);
+myChart.data.datasets.forEach((dataset) => {
+    dataset.data.push(dataSerial.value);
+
+    var ref = doc(db,"pHvalue", "pH");
+
+    updateDoc(
+       ref, {
+           pH: arrayUnion(dataSerial.value)
+        }
+       );
+    
+});
+counter++;
+myChart.update();
+});
